ac67c54bb200c823faa48aa6acdfa57450319d8f
Ramtin Abolfazli <Ramtina1381@gmail.com>
2025-04-04 19:17:11 -0400

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,23 @@
+// require("dotenv").config();
+// const express = require("express");
+// const { ApolloServer } = require("apollo-server-express");
+// const connectDB = require("./config/db");
+// const typeDefs = require("./graphql/typeDefs");
+// const resolvers = require("./graphql/resolvers");
+
+// connectDB();
+
+// const app = express();
+// const server = new ApolloServer({ typeDefs, resolvers });
+
+// async function startServer() {
+//   await server.start();
+//   server.applyMiddleware({ app });
+
+//   app.listen(4000, () => console.log("Server running on http://localhost:4000/graphql"));
+// }
+
+// startServer();
 require("dotenv").config();
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
@@ -10,35 +30,31 @@ connectDB();
 const app = express();
 const server = new ApolloServer({ typeDefs, resolvers });
 
-async function startServer() {
-  await server.start();
-  server.applyMiddleware({ app });
+let apolloServerStarted = false;
 
-  app.listen(4000, () => console.log("Server running on http://localhost:4000/graphql"));
+async function startApolloServer() {
+  await server.start();
+  server.applyMiddleware({ app, path: '/api/graphql' }); // Specify the /api/graphql path
+  apolloServerStarted = true;
 }
 
-startServer();
-// require("dotenv").config();
-// const express = require("express");
-// const { ApolloServer } = require("apollo-server-express");
-// const connectDB = require("./config/db");
-// const typeDefs = require("./graphql/typeDefs");
-// const resolvers = require("./graphql/resolvers");
+async function initializeServer() {
+  if (!apolloServerStarted) {
+    await startApolloServer();
+  }
+}
 
-// const app = express();
-// const server = new ApolloServer({ typeDefs, resolvers });
+module.exports = async (req, res) => {
+  await initializeServer();
+  return app(req, res);
+};
 
-// async function startServer() {
-//   await server.start();
-//   server.applyMiddleware({ app });
-//   console.log(`Server running on http://localhost:${process.env.PORT || 4000}/graphql`);
-// }
+// Optional: Handle the root path if you want a message there
+app.get('/', (req, res) => {
+  res.send('GraphQL API is available at /api/graphql');
+});
 
-// // Vercel expects an exported async handler
-// module.exports = async (req, res) => {
-//   if (!server.listening) {
-//     await startServer();
-//   }
-//   return app(req, res); // Respond with the app's express request handler
-// };
-
+// Optional: Handle favicon requests
+app.get('/favicon.ico', (req, res) => {
+  res.status(204).end();
+});
