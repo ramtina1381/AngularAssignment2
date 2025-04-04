472fb75dd9bb6f92f95aab87100e2439f7e12ca3
Ramtin Abolfazli <Ramtina1381@gmail.com>
2025-04-04 19:10:49 -0400

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,3 @@
-// require("dotenv").config();
-// const express = require("express");
-// const { ApolloServer } = require("apollo-server-express");
-// const connectDB = require("./config/db");
-// const typeDefs = require("./graphql/typeDefs");
-// const resolvers = require("./graphql/resolvers");
-
-// connectDB();
-
-// const app = express();
-// const server = new ApolloServer({ typeDefs, resolvers });
-
-// async function startServer() {
-//   await server.start();
-//   server.applyMiddleware({ app });
-
-//   app.listen(4000, () => console.log("Server running on http://localhost:4000/graphql"));
-// }
-
-// startServer();
 require("dotenv").config();
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
@@ -25,20 +5,40 @@ const connectDB = require("./config/db");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
+connectDB();
+
 const app = express();
 const server = new ApolloServer({ typeDefs, resolvers });
 
 async function startServer() {
   await server.start();
   server.applyMiddleware({ app });
-  console.log(`Server running on http://localhost:${process.env.PORT || 4000}/graphql`);
+
+  app.listen(4000, () => console.log("Server running on http://localhost:4000/graphql"));
 }
 
-// Vercel expects an exported async handler
-module.exports = async (req, res) => {
-  if (!server.listening) {
-    await startServer();
-  }
-  return app(req, res); // Respond with the app's express request handler
-};
+startServer();
+// require("dotenv").config();
+// const express = require("express");
+// const { ApolloServer } = require("apollo-server-express");
+// const connectDB = require("./config/db");
+// const typeDefs = require("./graphql/typeDefs");
+// const resolvers = require("./graphql/resolvers");
+
+// const app = express();
+// const server = new ApolloServer({ typeDefs, resolvers });
+
+// async function startServer() {
+//   await server.start();
+//   server.applyMiddleware({ app });
+//   console.log(`Server running on http://localhost:${process.env.PORT || 4000}/graphql`);
+// }
+
+// // Vercel expects an exported async handler
+// module.exports = async (req, res) => {
+//   if (!server.listening) {
+//     await startServer();
+//   }
+//   return app(req, res); // Respond with the app's express request handler
+// };
 
